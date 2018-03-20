db34b79b4b2faffffac5fd8ee4dd13af5e6e412a
matuseviciute <agnemane@gmail.com>
2018-03-20 17:43:10 +0100

    ...

--- tests/test_autoCorrect.py
+++ tests/test_autoCorrect.py
@@ -7,7 +7,7 @@ class TestEndToEnd(unittest.TestCase):
 
     def test_end_to_end(self):
         counts = numpy.random.negative_binomial(n = 20, p=0.2, size = (10,8))
-        sf = numpy.ones((10,1))
+        sf = numpy.ones((10,8))
         corrector = autoCorrect.correctors.AECorrector()
         corrector.correct(counts = counts, size_factors = sf)
         self.assertEqual(counts.shape, correction.shape)
